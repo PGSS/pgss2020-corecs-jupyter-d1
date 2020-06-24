c405c874057f0a277e5af36e29ef901d474dc302
syreal17 <ltjones@acm.org>
2020-06-24 17:20:40 +0000

    Added interactive code bit

---
+++ demystified-code.py
@@ -0,0 +1,22 @@
+
+def choose_greatest():
+    """Echo the largest of the two numbers given by the user to the program
+    
+    Warning: this function was written quickly and needs proper error handling
+    """
+
+    user_value_1 = input("What's your first number? ")
+    user_value_2 = input("What's your second number? ")
+    greatest_value = user_value_1 # need a value to return if 1 & 2 are equal
+
+    if user_value_1 > user_value_2:
+        greatest_value = user_value_1
+    elif user_value_1 < user_value_2:
+        greatest_value = user_value_2
+
+    print( "The number with largest positive magnitude is " 
+        + str(greatest_value) )
+
+
+
+choose_greatest() # <-- runs the function defined above.
