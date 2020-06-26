fad6f058e60d89d24e257c55d318699ce8231112
syreal17 <ltjones@acm.org>
2020-06-26 20:05:22 +0000

    Add backup method to interact with code

---
+++ important-code.copypasta.py
@@ -0,0 +1,46 @@
+# Reference alphabet
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+code = """
+# Hiding this really important number in an obscure piece of code is brilliant!
+# AND it's encrypted!
+# We want our biggest client to know his information is safe with us.
+bezos_cc_secret = "_aeg\\e`aa\\bg`g\\`beg"
+
+
+def decode_secret(secret):
+    
+    # Encryption key
+    rotate_const = 47
+    
+    # Storage for decoded secret
+    decoded = ""
+    
+    # decode loop
+    for c in secret:
+        index = alphabet.find(c)
+        original_index = (index + rotate_const) % len(alphabet)
+        decoded = decoded + alphabet[original_index]
+        
+    print(decoded)
+
+
+
+def choose_greatest():
+
+    user_value_1 = input("What's your first number? ")
+    user_value_2 = input("What's your second number? ")
+    greatest_value = user_value_1 # need a value to return if 1 & 2 are equal
+
+    if user_value_1 > user_value_2:
+        greatest_value = user_value_1
+    elif user_value_1 < user_value_2:
+        greatest_value = user_value_2
+
+    print( "The number with largest positive magnitude is " 
+        + str(greatest_value) )
+
+
+
+choose_greatest()
+"""
+exec(code)
