a9d418894969bdcda758928a22c4734c4d50c177
syreal17 <ltjones@acm.org>
2020-06-28 20:14:05 +0000

    Add whitespace

--- important-code.py
+++ important-code.py
@@ -6,6 +6,8 @@ bezos_cc_secret = "_aeg\\e`aa\\bg`g\\`beg"
 # Reference alphabet
 alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
+
+
 def decode_secret(secret):
     """ROT47 decode
     
